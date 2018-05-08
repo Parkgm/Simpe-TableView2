b47cf6986330cea006624a7fd32429feaa1d90f6
D7703_11 <d7703_11@d7703-11ui-imac.local>
2018-05-08 15:37:07 +0900

    tableView + alertController

--- Simple TableView/ViewController.swift
+++ Simple TableView/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var myTableView: UITableView!
     var animals = ["Cow", "Pig", "Dog", "Rabbit", "Bird"]
     var country = ["KOR", "USA", "Japan", "Africa", "China"]
+    var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,15 +52,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let identifier = "Cell"
         let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         
+        if indexPath.section == 0 {
+            let myImage = UIImage(named:"cat.png")
+            cell.imageView?.image = myImage
+        } else {
+            let myImage = UIImage(named:"monkey.jpeg")
+            cell.imageView?.image = myImage
+        }
+        
         // cell에 image 넣기
-        let myImage = UIImage(named:"cat.png")
-        cell.imageView?.image = myImage
+        
         
         // cell에 text 넣기
         cell.textLabel?.text = animals[indexPath.row]
         
         // cell에 detailText 넣기
-        cell.detailTextLabel?.text = country[indexPath.row]
+        cell.detailTextLabel?.text = String(indexPath.row)
         
         return cell
     }
@@ -74,10 +82,39 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         print("I selected \(mySection) Section \(myRow) Row")
         print(myAnimal)
         
-        let alert = UIAlertController(title: myAnimal, message: "Section \(mySection), Row \(myRow)", preferredStyle: .actionSheet)
+        let alert = UIAlertController(title: "동물농장", message: myAnimal, preferredStyle: .actionSheet)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default) {
+            (action:UIAlertAction) -> Void in
+            //alertController 생성
+            let myAlert2 = UIAlertController(title: "선택한것", message: "", preferredStyle: .alert)
+            
+            let okAction2 = UIAlertAction(title: "Section \(mySection) Row \(myRow)", style: .default , handler: nil)
+            
+            let cancelAction2 = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            
+            
+            myAlert2.addAction(okAction2)
+            myAlert2.addAction(cancelAction2)
+//            myAlert2.addAction(deleteAction2)
+            
+            self.present(myAlert2, animated: true, completion: nil)
+            
+            
+        }
+            
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
         alert.addAction(cancelAction)
+        alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
+    }
+    
+    //cell 의 높이 설정
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        count = count + 1
+        print(count)
+        return 100.0
     }
 }
 
